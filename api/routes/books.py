0410c091aad5fe6f0c93ac29eca2e9f3ec5de549
David <davidnduonofit47@gmail.com>
2025-02-11 23:49:32 +0100

    Implemented /api/v1/books/{book_id} endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,6 +48,16 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
+@router.get("/{book_id}")
+async def get_sepecific_book(book_id : int) -> Book:
+    book = db.books.get(book_id)
+    if book is None:
+        detail = {'detail' : 'Book not found'}
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=detail)
+    return book
+
+
+
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
     return JSONResponse(
