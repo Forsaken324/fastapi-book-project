2f760f60b409e1c8b1662e294addbed7dfa0ea39
David324325 <121452311+Forsaken324@users.noreply.github.com>
2025-02-14 05:18:11 +0100

    Update router.py

--- api/router.py
+++ api/router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from api.routes import books, stage2
+from api.routes import books
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
-api_router.include_router(stage2.app, tags=['stage2'])
+
 
 
