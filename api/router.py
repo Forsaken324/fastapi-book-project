457926cd943eee6b2ad223c89336353578dbf768
David324325 <121452311+Forsaken324@users.noreply.github.com>
2025-02-14 04:13:48 +0100

    Update router.py

--- api/router.py
+++ api/router.py
@@ -4,4 +4,4 @@ from api.routes import books
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
-api_router.include_router(stage2.app, tags=['stage2'])
+
