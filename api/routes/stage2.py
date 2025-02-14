13c43f27cb6fdd3a5a0a4da54cef507750562a3f
David324325 <121452311+Forsaken324@users.noreply.github.com>
2025-02-14 05:18:29 +0100

    Delete api/routes/stage2.py

--- api/routes/stage2.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, status
-from fastapi.responses import JSONResponse
-
-app = APIRouter()
-
-@app.get('/stage2')
-async def stage2():
-    return {'message' : 'welcome to stage 4000'}
